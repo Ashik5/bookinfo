0de14c2cfe674cba5b05775fff5cd68824dbc991
Ashik5 <48068370+Ashik5@users.noreply.github.com>
2025-01-20 13:49:24 +0600

    all done

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -37,7 +37,13 @@ const config = {
         }
       }
     ],
-    "@typescript-eslint/no-unsafe-member-access": "off"
+    "@typescript-eslint/no-unsafe-member-access": "off",
+    "@typescript-eslint/no-unsafe-argument": "off",
+    "@typescript-eslint/prefer-nullish-coalescing": "off",
+    "@typescript-eslint/no-unsafe-assignment": "off",
+    "@typescript-eslint/no-unsafe-return": "off",
+    "@typescript-eslint/no-unsafe-call": "off",
+    "@typescript-eslint/no-explicit-any": "off",
   }
 }
 
