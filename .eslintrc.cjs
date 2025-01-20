8019c61e67388ec031d6dffbb4bc6547a74857c6
Ashik5 <48068370+Ashik5@users.noreply.github.com>
2025-01-20 14:48:20 +0600

    database

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -44,6 +44,7 @@ const config = {
     "@typescript-eslint/no-unsafe-return": "off",
     "@typescript-eslint/no-unsafe-call": "off",
     "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-floating-promises": "off",
   }
 }
 
